1bdd26597524e17513e582f73b520c7321287fb8
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-14 21:40:55 +0300

    imported flask extensions and classes from models

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort, g
 from . import main
-from flask_login import login_required
-# from .models import review
-# from .forms import ReviewForm
+from .. import db
+from .models import User,Blog,Comment
+from flask_login import login_required, current_user
+from .forms import BlogForm
+from ..email import mail_message
 
 # Views
 @main.route('/')
