d754be6f21d853aaa3aa929184a5960b7d660997
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-14 21:15:12 +0300

    added admin column which is a boolean

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255),index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     email = db.Column(db.String(255),unique = True,index = True)
+    admin = db.Column(db.Boolean,default = False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
