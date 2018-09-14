563be9d3184a0166a8f5abdea2bf4e0a05b98c77
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-14 21:09:25 +0300

    created LoginForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField, BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
+
 
 class RegistrationForm(FlaskForm):
-  
+
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(),
@@ -18,7 +20,8 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is already taken')
+            raise ValidationError('That username is taken')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
