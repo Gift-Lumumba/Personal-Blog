f18182c25b7d0cf8589c17fb369ce86b5f5c1f23
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-14 12:13:59 +0300

    added decorator login_required that intercepts a request and check if user is authenticated and if not the user will be redirected to the login page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 # from .models import review
 # from .forms import ReviewForm
 
@@ -12,3 +13,22 @@ def index():
     '''
     title = 'Home - Welcome to Personal Blog'
     return render_template('index.html',title = title)
+
+@main.route('/blog/new/', methods = ['GET','POST'])
+@login_required
+def new_blog(id):
+
+    # form = BlogForm()
+    # # if category is None:
+    # #     abort( 404 )
+
+    # if form.validate_on_submit():
+    #     blog= form.content.data
+    #     # category_id = form.category_id.data
+    #     new_blog= (pitch= pitch)
+    
+
+    #     new_pitch.save_pitch()
+    #     return redirect(url_for('main.index'))
+
+    # return render_template('new_pitch.html', new_pitch_form= form, category= category)
