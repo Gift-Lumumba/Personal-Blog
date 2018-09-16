a7b0e5e38b3b4b8424414e1f540bb3ea69cda899
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-16 21:56:00 +0300

    imported unittest and imported classes from models

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
+import unittest
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User,Role,Blog,Comment,Category,Subscribe
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -15,16 +16,16 @@ manager.add_command('server',Server)
 
 @manager.command
 def test():
-    """Run the unit tests."""
+    """Run the unittests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role)
+    return dict(app = app,db = db,User = User,Role = Role,Blog = Blog,Comment = Comment,Category = Category,Subscribe = Subscribe)
 
 
 if __name__ == '__main__':
-    app.secret_key = 'gL0711'
+    # app.secret_key = 'gL0711'
     manager.run()
