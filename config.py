227d8b1e2173054694c5618776c6d0b83a37967a
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-16 21:55:12 +0300

    added SQLALCHEMY_DATABASE_URI on DevConfig

--- config.py
+++ config.py
@@ -4,24 +4,39 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gift:gL0711@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USER_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://gift:gL0711@localhost/blog'
 
     DEBUG = True
 
+class TestConfig(Config):
+    '''
+    child test configuration class
+    '''
+    pass
+
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
